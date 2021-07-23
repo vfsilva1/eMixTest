297233092e8f0b59c593f4157787c3046f4a4ca0
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2021-07-23 17:27:22 -0300

    Adding new CEP and list all working

---
+++ CEPCounsult/Controllers/CEPController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CEPCounsult.Data;
+using CEPCounsult.Models;
+
+namespace CEPCounsult.Controllers
+{
+    public class CEPController : Controller
+    {
+        private readonly CEPContext _context;
+
+        public CEPController(CEPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CEPs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CEPs.ToListAsync());
+        }
+
+        // GET: CEPs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cEP = await _context.CEPs
+                .FirstOrDefaultAsync(m => m.CepID == id);
+            if (cEP == null)
+            {
+                return NotFound();
+            }
+
+            return View(cEP);
+        }
+
+        // GET: CEPs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CEPs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CepID,Cep,Logradouro,Complemento,Bairro,Localidade,UF,Unidade,IBGE,GIA")] CEP cep)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cep);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cep);
+        }
+
+        // GET: CEPs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cEP = await _context.CEPs.FindAsync(id);
+            if (cEP == null)
+            {
+                return NotFound();
+            }
+            return View(cEP);
+        }
+
+        // POST: CEPs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CepID,Cep,Logradouro,Complemento,Bairro,Localidade,UF,Unidade,IBGE,GIA")] CEP cEP)
+        {
+            if (id != cEP.CepID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cEP);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CEPExists(cEP.CepID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cEP);
+        }
+
+        // GET: CEPs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cEP = await _context.CEPs
+                .FirstOrDefaultAsync(m => m.CepID == id);
+            if (cEP == null)
+            {
+                return NotFound();
+            }
+
+            return View(cEP);
+        }
+
+        // POST: CEPs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cEP = await _context.CEPs.FindAsync(id);
+            _context.CEPs.Remove(cEP);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CEPExists(int id)
+        {
+            return _context.CEPs.Any(e => e.CepID == id);
+        }
+    }
+}
--- CEPCounsult/Controllers/HomeController.cs
+++ CEPCounsult/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using CEPCounsult.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CEPCounsult.Controllers
@@ -12,10 +15,12 @@ namespace CEPCounsult.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Data.CEPContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Data.CEPContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -23,9 +28,52 @@ namespace CEPCounsult.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(string cep)
+        {
+            while (cep.Length > 8 || cep.Length < 8)
+            {
+                Response.StatusCode = 404;
+                return View();
+            }
+
+            string viaCEPUrl = "https://viacep.com.br/ws/" + cep + "/json/";
+            string result = string.Empty;
+
+            using (WebClient client = new WebClient())
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                client.Encoding = Encoding.UTF8;
+                result = client.DownloadString(viaCEPUrl);
+            }
+
+            CEP cepObject = JsonConvert.DeserializeObject<CEP>(result);
+            _context.CEPs.Add(cepObject);
+            _context.SaveChanges();
+
+            Response.StatusCode = 200;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ListCEPs()
+        {
+            var ceps = _context.CEPs;
+
+            return View(ceps);
+        }
+
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public ViewResult View()
+        {
+            var ceps = _context.CEPs;
+
+            return View(ceps);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
